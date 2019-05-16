d388fa52925f44ee1b8f7a438024d88b0e3627cd
blackzero23 <a46467734@gmail.com>
2019-05-16 17:33:09 +0900

    Study

--- Shutta/Shutta/Program.cs
+++ Shutta/Shutta/Program.cs
@@ -8,15 +8,17 @@ namespace Shutta
 {
     class Program
     {
-        public const int SeedMoney = 500;
-        private const int BettingMoney = 100;
+        public const int SeedMoney = 10000;
+        //private const int BettingMoney = 100;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("룰 타입을 선택하세요. (1:Basic 2:Simple");
+            Console.WriteLine("룰 타입을 선택하세요. (1:Basic 2:Simple)");
             int input = int.Parse(Console.ReadLine());
             RuleType rlueType = (RuleType)input;
 
+            Console.WriteLine("판돈 금액을 정하세요. 최대(1000원)");
+            int BettingMoney = int.Parse(Console.ReadLine());
 
             //오버라이드를 쓰는 전형적인 패턴
             List<Player> players = new List<Player>();
@@ -34,13 +36,17 @@ namespace Shutta
                     player.Money = SeedMoney;
 
             int round = 1;
-            while(true)
+
+            int keepBattingMoney = 0;
+            while (true)
             {
                 //한명이 오링이 면 게임 종료
                 if (isAnyoneOring(players))
                     break;
 
-                Console.WriteLine($"Round {round}" );
+
+
+                Console.WriteLine($"Round {round}");
                 round++;
 
 
@@ -51,7 +57,8 @@ namespace Shutta
                 foreach (var player in players)
                 {
                     player.Money -= BettingMoney;
-                    dealer.PutMoney(BettingMoney);
+                    dealer.PutMoney(BettingMoney + keepBattingMoney);
+                    keepBattingMoney = 0;
                 }
 
                 //카드 돌리기.
@@ -70,6 +77,14 @@ namespace Shutta
 
                 //승자 찾기
                 Player winner = FindWinner(players);
+
+                //무승부라면.
+                if (winner == null)
+                {
+                    Console.WriteLine("무승부!"+"\n");
+                    keepBattingMoney += dealer.GetMoney();
+                    continue;
+                }
 
                 //승자에게 상금 주기
                 winner.Money += dealer.GetMoney();
@@ -90,13 +105,35 @@ namespace Shutta
         private static Player FindWinner(List<Player> players)
         {
             int score0 = players[0].CaculateScore();
-            int score1 = players[1].CaculateScore();
+            //int score1 = players[1].CaculateScore();
 
-            //무승부 구현 해야됨.
-            if (score0 > score1)
-                return players[0];
-            else
-                return players[1];
+
+            ////무승부 구현 해야됨.
+            //if (score0 > score1)
+            //    return players[0];
+            //else if (score0 > score1)
+            //    return players[1];
+            //else
+            //    return null;
+
+
+            int[] score = new int[players.Count];
+            int[] rank = new int[players.Count];
+
+            Array.Clear(rank, 1, rank.Length);
+
+        
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                for (int j = 0; j < players.Count; j++)
+                      if (score[i] < score[j])
+                        rank[i]++;
+            }
+
+            score.Reverse();
+
+            return null;
 
         }
 
